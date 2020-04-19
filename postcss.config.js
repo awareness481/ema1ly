1205d3492ab8640a0c103b2a0a52ee2b9f67ef27
Jesse Jafa <jesse.jafa@gmail.com>
2020-04-19 04:59:36 +0300

    ema1ly initial

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  syntax: 'postcss-scss',
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+
+  ]
+
+}
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='w-10 bg-blue-500'>
+        
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
